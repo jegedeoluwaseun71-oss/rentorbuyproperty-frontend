2485e26c2fa9d621eaddffb98d15d406c4625778
jegedeoluwaseun71-oss <jegedeoluwaseun71@gmail.com>
2026-01-15 14:30:51 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1 style={{ color: "green" }}>RentOrBuyProperty</h1>
+      <p>Find verified properties for rent or sale</p>
+      <p>Listings coming soon...</p>
+    </div>
+  );
+  }
